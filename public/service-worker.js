524809b2879425181842d9c78e2b38698e9db068
Connor <melty100@gmail.com>
2020-11-03 18:42:53 -0800

    added favicon to mainfiest

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "./index.js",
+    "./favicon.ico",
     "./index.html",
     "./styles.css",
     "./manifest.webmanifest",
